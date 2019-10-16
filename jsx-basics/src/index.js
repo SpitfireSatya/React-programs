28cec85a7f0b9e533dd8d0aa4fb02a03198bc368
Spitfire <twisterintwist@gmail.com>
2019-10-16 00:22:32 -0400

    [jsx-basics] Added scaffolding for jsx-basics react app

---
+++ jsx-basics/src/index.js
@@ -0,0 +1,12 @@
+
+// import react and react-dom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a react component
+const App = () => {
+  return <div> Hi There! </div>
+}
+
+// show the react component
+ReactDOM.render(<App />, document.getElementById('root'));
