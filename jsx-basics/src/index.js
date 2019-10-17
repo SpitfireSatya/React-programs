325f889dbde687119c6f27847dcd9bdf7b9efc57
Spitfire <twisterintwist@gmail.com>
2019-10-16 20:35:47 -0400

    [jsx-basics] jsx basic concepts

--- jsx-basics/src/index.js
+++ jsx-basics/src/index.js
@@ -5,7 +5,35 @@ import ReactDOM from 'react-dom';
 
 // create a react component
 const App = () => {
-  return <div> Hi There! </div>
+
+  function getButtonText() {
+    return "Click on me too!"
+  }
+
+  let buttonText = "Click me!"
+  let numbersWork = 12345;
+  let arraysWork = ["HI", "THERE"];
+  // Use parenthesis for multiline html as a convention
+  // use {{}} for properties and enter them as a json. Use camel case for variables.
+  // Use {} for bindings/interpolation
+  // Use htmlFor instead of for (in label)
+  return (
+    <div>
+      <label className="label" htmlFor="name">Enter Name</label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white"}}>
+        {buttonText}
+      </button>
+      <button style={{ backgroundColor: "blue", color: "white"}}>
+        {getButtonText()}
+      </button>
+      <div>{numbersWork}</div>
+      <div>{arraysWork}</div>
+
+      <div>But Objects dont work!!</div>
+
+    </div>
+  )
 }
 
 // show the react component
