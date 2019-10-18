b3ca87e571a006b834de2f95d588d31f89d9423a
Spitfire <twisterintwist@gmail.com>
2019-10-18 00:58:17 -0400

    [component-basics] Adding approval card component and passing comment detail as a child component

--- component-basics/src/index.js
+++ component-basics/src/index.js
@@ -2,14 +2,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './comment-detail';
+import ApprovalCard from './approval-card';
 import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" avatar={faker.image.avatar()} content="Good Job!" />
-      <CommentDetail author="Alex" timeAgo="Today at 3:00 PM" avatar={faker.image.avatar()} content="Nice blog!" />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 6:00 PM" avatar={faker.image.avatar()} content="Awesome work!" />
+
+      <ApprovalCard>
+        <h4> Warning!! </h4>
+        <div>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" avatar={faker.image.avatar()} content="Good Job!" />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 3:00 PM" avatar={faker.image.avatar()} content="Nice blog!" />
+      </ApprovalCard>
+    
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Yesterday at 6:00 PM" avatar={faker.image.avatar()} content="Awesome work!" />
+      </ApprovalCard>
+
     </div>
   )
 }
