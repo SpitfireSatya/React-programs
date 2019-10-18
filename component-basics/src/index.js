05dfdd784a7d09ced1da8becdf9e038b195c628b
Spitfire <twisterintwist@gmail.com>
2019-10-18 00:42:10 -0400

    [component-basics] Using props and completing comment detail component

--- component-basics/src/index.js
+++ component-basics/src/index.js
@@ -2,15 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './comment-detail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail></CommentDetail>
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" avatar={faker.image.avatar()} content="Good Job!" />
+      <CommentDetail author="Alex" timeAgo="Today at 3:00 PM" avatar={faker.image.avatar()} content="Nice blog!" />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 6:00 PM" avatar={faker.image.avatar()} content="Awesome work!" />
     </div>
   )
 }
