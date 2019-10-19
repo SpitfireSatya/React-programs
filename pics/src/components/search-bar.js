13a701cdb704ad0dc222390bf069c6f4d2ae3fd0
Spitfire <twisterintwist@gmail.com>
2019-10-19 11:39:17 -0400

    [pics] Adding onChange event handler with state to search bar component

--- pics/src/components/search-bar.js
+++ pics/src/components/search-bar.js
@@ -3,13 +3,19 @@ import React from 'react'
 
 class SearchBar extends React.Component {
 
+  state = { term: 'Search Something' };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className=" ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
           </div>
         </form>
       </div>
