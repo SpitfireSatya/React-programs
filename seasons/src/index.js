4b53939788971f54c0179422cc32dc5296183bc8
Spitfire <twisterintwist@gmail.com>
2019-10-18 22:01:16 -0400

    [seasons] Adding SeasonDisplay component and geolocation

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,7 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+
+  window.navigator.geolocation.getCurrentPosition((pos) => {
+    console.log(pos);
+  }, (error) => {
+    console.log('User denied location', error)
+  });
+
   return (<div>Hi There!</div>)
 }
 
