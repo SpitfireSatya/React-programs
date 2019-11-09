ee68055081253beff784d865786fe6a0405ad593
Spitfire <twisterintwist@gmail.com>
2019-11-08 23:15:47 -0500

    [streams] Implementing basic routing for 2 paths

--- streams/client/src/components/app.js
+++ streams/client/src/components/app.js
@@ -1,12 +1,28 @@
 
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>Page One</div>
+  );
+}
+
+const PageTwo = () => {
+  return (
+    <div>Page Two</div>
+  );
+}
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        App Component
+        <BrowserRouter>
+          <Route path="/" exact component={PageOne} />  
+          <Route path="/pagetwo" exact component={PageTwo} />
+        </BrowserRouter>
       </div>
     );
   }
