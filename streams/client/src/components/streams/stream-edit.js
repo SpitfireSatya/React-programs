783f1a56b17506cb5036795c0e1dbd0bd1aa93af
Spitfire <twisterintwist@gmail.com>
2019-11-17 13:53:12 -0500

    [streams] Refactoring stream create and edit components. Using patch for edit instead of post

--- streams/client/src/components/streams/stream-edit.js
+++ streams/client/src/components/streams/stream-edit.js
@@ -1,16 +1,40 @@
 
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './stream-form';
 
 class StreamEdit extends React.Component {
 
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  }
+
   render() {
+    if(!this.props.stream) {
+      return <div>Loading...</div>
+    }
     return(
       <div>
-        StreamEdit
+        <h3>Edit a Stream</h3>
+        <StreamForm initialValues={{
+            title: this.props.stream.title,
+            description: this.props.stream.description
+          }}
+          onSubmit={this.onSubmit} />
       </div>
     )
   }
 
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps,
+  { fetchStream, editStream })(StreamEdit);
