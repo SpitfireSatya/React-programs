77bd82fdcb5684701bd92307b2314f0f2d03a527
Spitfire <twisterintwist@gmail.com>
2019-11-17 15:15:10 -0500

    [streams] Some more minor changes

--- streams/client/src/components/app.js
+++ streams/client/src/components/app.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamList from './streams/stream-list';
 import StreamCreate from './streams/stream-create';
 import StreamEdit from './streams/stream-edit';
@@ -16,11 +16,15 @@ class App extends React.Component {
       <div className="ui container">
         <Router history={history}>
           <Header />
-          <Route path="/" exact component={StreamList} />  
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} />
+          
+          <Switch>
+            <Route path="/" exact component={StreamList} />  
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
+          
         </Router>
       </div>
     );
