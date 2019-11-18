47b004048902362de86775ec8c2a31796bf49db5
Spitfire <twisterintwist@gmail.com>
2019-11-18 01:57:42 -0500

    [seasons] Refactoring code to use hooks

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,52 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './season-display'
 import Spinner from './spinner';
+import userLocation from './user-location';
 
-class App extends React.Component {
+const App = () => {
 
-  state = { lat: null, errorMessage: '' };
-
-  constructor(props) {
-    
-    console.log('Constructor executed');
-
-    super(props)
-
+  const [lat, errorMessage] = userLocation();
+  
+  let content;
+  if(errorMessage && !lat) {
+    content = <div>Error: {errorMessage}</div>
+  } else if(!errorMessage && lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Spinner message="Please accept the location request" />
   }
 
-  componentWillMount() {
-    console.log('Component will mount executed');
-  }
-
-  componentDidMount() {
-    console.log('Component did mount executed');
-    window.navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({ lat: position.coords.latitude });
-    }, (error) => {
-      this.setState({ errorMessage: error.message });
-    });
-  }
-
-  componentDidUpdate() {
-    console.log('Component did update executed');
-  }
-
-  componentWillUnmount() {
-    console.log('Component will unmount executed');
-  }
-
-  render() {
-
-    console.log('Render executed');
-
-    if(this.state.errorMessage && !this.state.lat) {
-      return (<div>Error: {this.state.errorMessage}</div>);
-    } else if(!this.state.errorMessage && this.state.lat) {
-      return (<SeasonDisplay lat={this.state.lat} />);
-    } else {
-      return (<Spinner message="Please accept the location request" />);
-    }
-  }
+  return (<div className="border red">{content}</div>);
 
 }
 
